0fff71f64579037eb206732228e655abc5a82b6c
paul <paulieman73@gmail.com>
2022-10-20 22:56:09 +0400

    Save graph to file

--- app/src/main/java/ru/pauliesoft/test/ui/result/tabs/GraphFragment.kt
+++ app/src/main/java/ru/pauliesoft/test/ui/result/tabs/GraphFragment.kt
@@ -1,6 +1,12 @@
 package ru.pauliesoft.test.ui.result.tabs
 
+import android.content.ContentValues
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,10 +15,16 @@ import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import dagger.hilt.android.AndroidEntryPoint
+import ru.pauliesoft.test.R
 import ru.pauliesoft.test.databinding.FragmentGraphBinding
+import ru.pauliesoft.test.showSnackBar
 import ru.pauliesoft.test.ui.MainViewModel
 import ru.pauliesoft.test.ui.base.BaseFragment
 import ru.pauliesoft.test.ui.mappers.Point
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
+
 
 @AndroidEntryPoint
 class GraphFragment : BaseFragment() {
@@ -30,6 +42,7 @@ class GraphFragment : BaseFragment() {
     override fun setupViews() {
         binding.radioButtonStraight.setOnClickListener { viewModel.isGraphSmooth.postValue(false) }
         binding.radioButtonSmooth.setOnClickListener { viewModel.isGraphSmooth.postValue(true) }
+        binding.saveToFileButton.setOnClickListener { saveMediaToStorage(binding.lineChart.chartBitmap) }
     }
 
     override fun setupObservers() {
@@ -57,6 +70,50 @@ class GraphFragment : BaseFragment() {
                 }
             })
             lineChart.invalidate()
+        }
+    }
+
+    private fun saveMediaToStorage(bitmap: Bitmap) {
+        //Generating a file name
+        val filename = "${System.currentTimeMillis()}.jpg"
+
+        //Output stream
+        var fos: OutputStream? = null
+
+        //For devices running android >= Q
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            //getting the contentResolver
+            context?.contentResolver?.also { resolver ->
+
+                //Content resolver will process the contentvalues
+                val contentValues = ContentValues().apply {
+
+                    //putting file information in content values
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+                    put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+                    put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                }
+
+                //Inserting the contentValues to contentResolver and getting the Uri
+                val imageUri: Uri? =
+                    resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+                //Opening an outputstream with the Uri that we got
+                fos = imageUri?.let { resolver.openOutputStream(it) }
+            }
+        } else {
+            //These for devices running on android < Q
+            //So I don't think an explanation is needed here
+            val imagesDir =
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            val image = File(imagesDir, filename)
+            fos = FileOutputStream(image)
+        }
+
+        fos?.use {
+            //Finally writing the bitmap to the output stream that we opened
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it)
+            showSnackBar(binding.root, getString(R.string.file_saved))
         }
     }
 }
